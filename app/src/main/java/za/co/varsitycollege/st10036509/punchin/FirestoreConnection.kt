d5889b7db3f1b3bb4bd87151c9056053f758e032
EthanSchoonbee <schoonbeeethan@gmail.com>
2024-04-23 13:50:16 +0200

    OS-142 : Added Relevant Comments to FirestoreConnection Class

--- app/src/main/java/za/co/varsitycollege/st10036509/punchin/FirestoreConnection.kt
+++ app/src/main/java/za/co/varsitycollege/st10036509/punchin/FirestoreConnection.kt
@@ -1,12 +1,24 @@
+/*
+AUTHOR: Ethan Schoonbee
+CREATED: 23/04/2024
+LAST MODIFIED: 23/04/2024
+ */
+
 package za.co.varsitycollege.st10036509.punchin
 
 import com.google.firebase.firestore.FirebaseFirestore
-///
-// Singleton Class (object) for connecting to the firebase cloud database
-///
-object FirestoreConnection {
-    private val firestoreInstance: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
 
+/**
+ * Singleton Object to handle the fire database connection
+ */
+object FirestoreConnection {
+
+    private val firestoreInstance: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }//get firestore instance through thread-safe lazy call
+
+    /**
+     * Method to fetch the firestore instance
+     * @return FirebaseFirestore instance
+     */
     fun getFirestoreInstance(): FirebaseFirestore {
         return firestoreInstance
     }
