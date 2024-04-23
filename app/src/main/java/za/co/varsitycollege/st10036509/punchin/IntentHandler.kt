c6487055deff24de24ff3129e67c1db93885c551
EthanSchoonbee <schoonbeeethan@gmail.com>
2024-04-23 13:42:32 +0200

    OS-134 : Added IntentHandler Class and Functionality for Basic Activity Navigation

---
+++ app/src/main/java/za/co/varsitycollege/st10036509/punchin/IntentHandler.kt
@@ -0,0 +1,27 @@
+/*
+AUTHOR: Ethan Schoonbee
+CREATED: 23/04/2024
+LAST MODIFIED: 23/04/2024
+ */
+
+package za.co.varsitycollege.st10036509.punchin
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+
+/**
+ * Class to handle intents
+ * @param Context The Current Application Context.
+ */
+class IntentHandler(private val context: Context) {
+    /**
+     * Method to handle basic activity navigation with no data
+     * @param Class<*> The Class name of the Activity to open.
+     */
+    fun openActivityIntent(activity: Class<*>) {
+        val intent = Intent(context, activity)//create intent object
+        context.startActivity(intent)//open activity
+    }
+}
+//______________________....oooOO0_END_OF_FILE_0OOooo....______________________\\
