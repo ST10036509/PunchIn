e89918075025c3ddd3d151c54ab594cbf2574dbe
EthanSchoonbee <schoonbeeethan@gmail.com>
2024-04-23 12:19:13 +0200

    Added AppCompatActivity Dependencies

---
+++ app/src/main/java/za/co/varsitycollege/st10036509/punchin/LoginActivity.kt
@@ -0,0 +1,12 @@
+package za.co.varsitycollege.st10036509.punchin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
