e162169bda7ffe94ea4d1f199be4054dbf071b2f
EthanSchoonbee <schoonbeeethan@gmail.com>
2024-04-27 18:27:41 +0200

    OS-166 : User Registration Functionality Implemented

--- app/src/main/java/za/co/varsitycollege/st10036509/punchin/AuthenticationViewModel.kt
+++ app/src/main/java/za/co/varsitycollege/st10036509/punchin/AuthenticationViewModel.kt
@@ -1,61 +1,138 @@
+/*
+AUTHOR: Ethan Schoonbee
+CREATED: 26/04/2024
+LAST MODIFIED: 27/04/2024
+ */
+
 package za.co.varsitycollege.st10036509.punchin
 
-import android.content.Context
-import android.service.autofill.UserData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestoreException
+import kotlinx.coroutines.tasks.await
 
-class AuthenticationViewModel(context: Context) :ViewModel() {
 
-    private val authInstance: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+/**
+ * Class to handle Authentication Functionality
+ */
+class AuthenticationViewModel() :ViewModel() {
 
-    fun signUp(email: String, password: String, username: String, callback: (Boolean) -> Unit) {
+    private val authInstance: FirebaseAuth by lazy { FirebaseAuth.getInstance() }//link an instance of the firebase authentication sdk
 
+    //strings to use
+    private companion object {
+        const val MSG_NULL_INPUTS_ERROR = "Please fill out all inputs!"
+        const val MSG_USERNAME_TAKEN_ERROR = "The user you entered is already taken!"
+        const val MSG_EMAIL_ERROR = "The email you entered is not valid!"
+        const val MSG_PASSWORD_MISSMATCH_ERROR = "The passwords do not match!"
+        const val MSG_PASSWORD_DIGIT_ERROR = "Password must contain at least one digit."
+        const val MSG_PASSWORD_LOWERCASE_ERROR = "Password must contain at least one lowercase letter."
+        const val MSG_PASSWORD_UPPERCASE_ERROR = "Password must contain at least one uppercase letter."
+        const val MSG_PASSWORD_LENGTH_ERROR = "Password must be at least 6 characters long."
+    }
+
+
+//__________________________________________________________________________________________________signUp
+
+
+    /**
+     * Method to create a user profile
+     * @param String email
+     * @param String password
+     * @param String username
+     * @return Pair<Boolean, String> callback
+     */
+    fun signUp(email: String, password: String, username: String, callback: (Pair<Boolean, String>) -> Unit) {
+
+        //run the firebase authentication method to create a new user account
         authInstance.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
+            .addOnCompleteListener { task -> //listen for success or failure
 
+                //if the account was successfully created
                 if (task.isSuccessful) {
 
+                    //get the user that was just created
                     val user = authInstance.currentUser
 
+                    //if the user exists
                     user?.let {
 
-                        val userData = hashMapOf (
+                        //create user context and assign default values
+                        val userData = User(username, 0,0,24)
 
-                            "username" to username,
-                            "points" to 0,
-                            "minGoal" to 0,
-                            "maxGoal" to 24
+                        //save the user data to the database
+                        saveAdditionalUserDataToFirestore(user.uid, userData) { success ->
 
-                        )
+                            //if the data is added to the database successfully
+                            if (success) {
 
-                        saveAdditionalUserDataToFirestore(user.uid, userData, callback)
+                                //return callback with no message
+                                callback(Pair(true, ""))
 
+                                //if the data is added to the database successfully
+                            } else {
+
+                                //return callback with no message
+                                callback(Pair(false, "Failed to add user data to database"))
+
+                            }
+                        }
                     }
 
+                //if an error occurred during the account creation
                 } else {
-                    callback(false)
+
+                    //fetch the task exception and get the error message
+                    val errorMessage = task.exception?.message?.let { message ->
+                        val parts = message.split(":")
+                        if (parts.size > 1) {
+                            parts[1].trim()
+                        } else {
+                            message.trim()
+                        }
+                    }
+
+                    //return callback with the unhandled error message
+                    callback(Pair(false, errorMessage ?: "Unknown Error Occurred"))
                 }
             }
     }
 
-    private fun saveAdditionalUserDataToFirestore(uid: String, userData: Map<String, Any>, callback: (Boolean) -> Unit) {
 
+//__________________________________________________________________________________________________saveAdditionalUserDataToFirestore
+
+
+    /**
+     * Method to save the additional data of a user to the database
+     * @param String user unique id
+     * @param HashMap additional user data
+     * @return Boolean callback
+     */
+    private fun saveAdditionalUserDataToFirestore(uid: String, userData: User, callback: (Boolean) -> Unit) {
+
+        //open the database connection and find/create the users collection
         FirestoreConnection.getDatabaseInstance().collection("users")
-            .document(uid)
-            .set(userData)
-            .addOnSuccessListener {
-                callback(true)
-            }
-            .addOnFailureListener {
-                callback(false)
-            }
+            .document(uid)//create a new document with user uid
+            .set(userData)//write the user data
+            .addOnSuccessListener { callback(true) }//if the data is successfully added
+            .addOnFailureListener { callback(false) }//if an error occurs while adding the extra user data
     }
 
+
+//__________________________________________________________________________________________________signIn
+
+
+    /**
+     * Method to log in user who already has an account
+     * @param String email or username
+     * @param String password
+     * @return Pair<Boolean, String> callback
+     */
     fun signIn(email: String, password: String, username: String, callback: (Boolean) -> Unit) {
 
+        //run the firebase authentication method to sign an user in
         authInstance.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener {task ->
+            .addOnCompleteListener {task -> //listen for success or failure
 
                 callback(task.isSuccessful)
 
@@ -63,10 +140,193 @@ class AuthenticationViewModel(context: Context) :ViewModel() {
 
     }
 
+
+//__________________________________________________________________________________________________signOut
+
+
+    /**
+     * Method to sign a logged in user from the app
+     */
     fun signOut() {
 
-        authInstance.signOut()
+        authInstance.signOut()//sign the user out
 
     }
 
+
+//__________________________________________________________________________________________________runValidationChecks
+
+
+    /**
+     * Method to check for inout validity
+     * @param String Username
+     * @param String Email
+     * @param String Password
+     * @param String PasswordConfirmation
+     * @return Boolean {true} there are no validation errors/ {false} there are validation errors
+     */
+    suspend fun runValidationChecks(username: String, email: String, password: String, passwordConfirmation: String): Pair<Boolean, String> {
+
+        val (isValidPasswordFormat, passwordFormatErrorMessage) = validatePasswordFormat(password)
+
+        //check if all inputs are valid
+        if (checkForNullInputs(username, email, password, passwordConfirmation)) {
+
+            return Pair(false, MSG_NULL_INPUTS_ERROR)
+
+        }
+
+        if (!validateUsername(username)) {
+
+            return Pair(false, MSG_USERNAME_TAKEN_ERROR)
+
+        }
+
+        //check if the email is in the correct format
+        if (!validateEmail(email)) {
+
+            return Pair(false, MSG_EMAIL_ERROR)
+
+        }
+
+        //check if the password is in the correct format
+        if (!isValidPasswordFormat) {
+
+            return Pair(false, passwordFormatErrorMessage)
+
+        }
+
+        //check if the password and password confirmation match
+        if (!validatePasswordsMatch(password, passwordConfirmation)) {
+
+            return Pair(false, MSG_PASSWORD_MISSMATCH_ERROR)
+
+        }
+
+        //if all inputs are valid
+        return Pair(true, "")
+
+    }
+
+
+//__________________________________________________________________________________________________checkForNullInputs
+
+
+    /**
+     * Method to check for null inputs
+     * @param String Username
+     * @param String Email
+     * @param String Password
+     * @param String PasswordConfirmation
+     * @return Boolean {true} there are no null values/ {false} there are null values present
+     */
+    private fun checkForNullInputs(username: String, email: String, password: String, passwordConfirmation: String): Boolean {
+
+        return (username.isEmpty() || email.isEmpty() || password.isEmpty() || passwordConfirmation.isEmpty())
+
+    }
+
+
+//__________________________________________________________________________________________________validateUsername
+
+
+    /**
+     * Method to check for null inputs
+     * @param String Username
+     * @return Boolean {true} username is unique/ {false} username already exists
+     */
+    private suspend fun validateUsername(username: String): Boolean {
+
+        val usersCollection = FirestoreConnection.getDatabaseInstance().collection("users")
+
+        return try {
+
+            val querySnapshot = usersCollection.whereEqualTo("username", username).get().await()
+            querySnapshot.isEmpty
+
+
+        } catch (e: FirebaseFirestoreException) {
+
+            false
+
+        }
+
+    }
+
+
+//__________________________________________________________________________________________________validateEmail
+
+
+    /**
+     * Method to validate the given Email
+     * @param String Email
+     * @return Boolean {true} email is valid/ {false} email is not valid
+     */
+    private fun validateEmail(email: String): Boolean {
+
+        //format for emails
+        val emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}\$"
+        return(email.matches(emailRegex.toRegex()))
+
+    }
+
+
+//__________________________________________________________________________________________________validatePasswordsMatch
+
+
+    /**
+     * Method to validate the passwords match
+     * @param String Password
+     * @param String PasswordConfirmation
+     * @return Boolean {true} passwords match/ {false} passwords don't match
+     */
+    private fun validatePasswordsMatch(password: String, passwordConfirmation: String): Boolean {
+
+        return(password == passwordConfirmation)
+
+    }
+
+
+//__________________________________________________________________________________________________validatePasswordFormat
+
+
+    /**
+     * Method to validate the password matches the required format
+     * @param String Password
+     * @return Boolean {true} passwords is in the correct format/ {false} passwords is not in the correct format
+     */
+    private fun validatePasswordFormat(password: String): Pair<Boolean, String> {
+
+        // Check if password meets Firebase Authentication requirements
+        val minLength = 6
+        val hasUpperCase = password.any { it.isUpperCase() }
+        val hasLowerCase = password.any { it.isLowerCase() }
+        val hasDigit = password.any { it.isDigit() }
+
+        // Validate password format based on requirements
+        if (password.length < minLength) {
+
+            return Pair(false, MSG_PASSWORD_LENGTH_ERROR)
+
+        }
+        if (!hasUpperCase) {
+
+            return Pair(false, MSG_PASSWORD_UPPERCASE_ERROR)
+
+        }
+        if (!hasLowerCase) {
+
+            return Pair(false, MSG_PASSWORD_LOWERCASE_ERROR)
+
+        }
+        if (!hasDigit) {
+
+            return Pair(false, MSG_PASSWORD_DIGIT_ERROR)
+
+        }
+
+        return Pair(true, "")
+
+    }
 }
+//______________________....oooOO0_END_OF_FILE_0OOooo....______________________\\
--- app/src/main/java/za/co/varsitycollege/st10036509/punchin/RegisterActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10036509/punchin/RegisterActivity.kt
@@ -6,14 +6,19 @@ LAST MODIFIED: 26/04/2024
 
 package za.co.varsitycollege.st10036509.punchin
 
+
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.text.method.PasswordTransformationMethod
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Toast
-import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import za.co.varsitycollege.st10036509.punchin.databinding.ActivityRegisterBinding
 
 
@@ -24,12 +29,14 @@ class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding//bind the RegisterActivities KT and XML files
     private lateinit var intentHandler: IntentHandler//setup an intent handler for navigating pages
-    private lateinit var authModel: AuthenticationViewModel
+    private lateinit var authModel: AuthenticationViewModel//setup an instance of the authentication context
+    private  var progressDialog: ProgressDialog? = null
 
     //constant strings for toast messages
     private companion object {
-        const val MSG_REGISTER_SUCCESS = "ACCOUNT REGISTERED SUCCESSFULLY"
-        const val MSG_REGISTER_FAILIURE = "<FAILED TO REGISTER USER>"
+        const val MSG_REGISTER_SUCCESS = "Account Registered Successfully!"
+        const val MSG_CHECKING_INPUTS = "Validating inputs..."
+        const val MSG_REGISTERING_USER = "Registering your account..."
     }
 
 //__________________________________________________________________________________________________onCreate
@@ -43,8 +50,8 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)//inflate UI
         setContentView(binding.root)
 
-        intentHandler = IntentHandler(this@RegisterActivity)
-        authModel = AuthenticationViewModel(this@RegisterActivity)
+        intentHandler = IntentHandler(this@RegisterActivity)//initialise the intentHandler
+        authModel = AuthenticationViewModel()//initialise the authentication context
 
         //setup listeners for ui controls
         setupListeners()
@@ -63,7 +70,7 @@ class RegisterActivity : AppCompatActivity() {
         //apply binding to the following lines
         binding.apply {
 
-            //pass appropriate message to toast
+            //run appropriate method when buttons are clicked
             llRegisterButton.setOnClickListener { registerUser() }
             llReturnButton.setOnClickListener { openLoginPage() }
 
@@ -76,42 +83,79 @@ class RegisterActivity : AppCompatActivity() {
     }
 
 
-//__________________________________________________________________________________________________openLoginPage
+//__________________________________________________________________________________________________registerUser
 
 
     /**
-     * Register the User.
-     * If successful launch the TimesheetsView page.
+     * Method to Register the User.
+     * If successful launch the TimesheetView page.
      * If unsuccessful show Error Message
      */
+    @OptIn(DelicateCoroutinesApi::class)
     private fun registerUser() {
+
+        //check if the username is already taken
+        GlobalScope.launch {
+            binding.apply {
+
+                //capture user inputs
+                val username = etUsername.text.toString().trim()
+                val email = etEmail.text.toString().trim()
+                val password = etPassword.text.toString().trim()
+                val passwordConfirmation = etPasswordConfirmation.text.toString().trim()
+
+                withContext(Dispatchers.Main) {
+
+                    showLoadingDialog(MSG_CHECKING_INPUTS)//display a loading dialog
+
+                    val (inputsValid, inputValidationErrorMessage) = authModel.runValidationChecks(
+                        username,
+                        email,
+                        password,
+                        passwordConfirmation
+                    )
+
+                    dismissLoadingDialog()
+
+                    //check if inputs are all valid
+                    if (inputsValid) {
+
+                        showLoadingDialog(MSG_REGISTERING_USER)//display a loading dialog
+                        //attempt to sign the user up
+                        authModel.signUp(
+                            email,
+                            password,
+                            username,
+                            ::handleSignUpCallBack
+                        )
+
+                    } else {
+
+                        showToast(inputValidationErrorMessage)
+
+                    }
+                }
+            }
+        }
+    }
+
+
+//__________________________________________________________________________________________________clearInputs
+
+
+    /**
+     * Method to clear inputs text
+     */
+    private fun clearInputs() {
 
         binding.apply {
 
-            var username = etUsername.text.toString()
-            var email = etEmail.text.toString()
-            var password = etPassword.text.toString()
-
-            //validateUsername()
-            //validateEmail()
-            //validatePassword()
-
-
-            authModel.signUp(email, password, username) {success ->
-
-                if (success) {
-
-                    showToast(MSG_REGISTER_SUCCESS)
-
-                } else {
-
-                    showToast(MSG_REGISTER_FAILIURE)
-
-                }
-            }
+            etUsername.text.clear()
+            etEmail.text.clear()
+            etPassword.text.clear()
+            etPasswordConfirmation.text.clear()
 
         }
-
 
     }
 
@@ -150,6 +194,7 @@ class RegisterActivity : AppCompatActivity() {
             toggleButton.setImageResource(
                 // if the password is visible, show eye open image else show a eye closed image
                 if (isPasswordVisible) R.drawable.eye_closed else R.drawable.eye_open
+
             )
             // reset cursor position to the end of the string
             inputBox.setSelection(inputBox.text.length)
@@ -158,7 +203,69 @@ class RegisterActivity : AppCompatActivity() {
     }
 
 
-    //__________________________________________________________________________________________________showToast
+//__________________________________________________________________________________________________handleSignUpCallBack
+
+
+    /**
+     * Method to handle callback when an unhandled serverside error occurs
+     * @param Pair<Boolean, String>
+     */
+
+    private fun handleSignUpCallBack(result: Pair<Boolean, String>) {
+
+        //if there were no errors
+        if (result.first) {
+
+            dismissLoadingDialog()//close loading icon
+            showToast(RegisterActivity.MSG_REGISTER_SUCCESS)//show success message
+            clearInputs()//clear input boxes
+
+            //if if there were no errors
+        } else {
+
+            dismissLoadingDialog()//close loading icon
+            showToast(result.second)//show given error message
+
+        }
+    }
+
+
+//__________________________________________________________________________________________________showLoadingDialog
+
+
+    /**
+     * Method to show loading dialog
+     */
+    private fun showLoadingDialog(message: String) {
+
+        progressDialog?.dismiss()
+
+        progressDialog = ProgressDialog(this@RegisterActivity).apply {
+
+            setTitle(message)
+            setCancelable(false)
+            show()
+
+        }
+    }
+
+
+//__________________________________________________________________________________________________dismissLoadingDialog
+
+
+    /**
+     * Method to discard loading dialog
+     */
+    private fun dismissLoadingDialog() {
+
+        progressDialog?.dismiss()
+
+        progressDialog = null
+
+    }
+
+
+//__________________________________________________________________________________________________showToast
 
 
     /**
