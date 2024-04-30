69acf2acffeee1f0ee9d2a401db8f5916a64c3d5
st10048758 <st10048758@vcconnect.edu.za>
2024-05-01 00:52:18 +0200

    LOTS has been added.

--- app/src/main/java/za/co/varsitycollege/st10036509/punchin/activities/TimesheetViewActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10036509/punchin/activities/TimesheetViewActivity.kt
@@ -2,21 +2,26 @@ package za.co.varsitycollege.st10036509.punchin.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import za.co.varsitycollege.st10036509.punchin.databinding.ActivityTimesheetViewBinding
+
 import android.widget.Button
 import android.widget.TextView
 import java.text.SimpleDateFormat
 import java.util.*
 import android.content.Intent
-import za.co.varsitycollege.st10036509.punchin.activities.GoalsActivity //TimesheetCreationActivity
+import android.util.Log
+import za.co.varsitycollege.st10036509.punchin.R
+import za.co.varsitycollege.st10036509.punchin.activities.TimesheetCreationActivity
+import za.co.varsitycollege.st10036509.punchin.databinding.ActivityTimesheetViewBinding
 import za.co.varsitycollege.st10036509.punchin.utils.FirestoreConnection
+import za.co.varsitycollege.st10036509.punchin.utils.IntentHandler
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class TimesheetViewActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityTimesheetViewBinding
-    private lateinit var btnPreviousWeek: Button
-    private lateinit var btnNextWeek: Button
-    private lateinit var tvWeeklySelector: TextView
+    private lateinit var binding: za.co.varsitycollege.st10036509.punchin.databinding.ActivityTimesheetViewBinding
     private val firestoreInstance = FirestoreConnection.getDatabaseInstance()
+    private lateinit var intentHandler: IntentHandler
+    private val currentDate = LocalDate.now()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,67 +39,79 @@ class TimesheetViewActivity : AppCompatActivity() {
         }
 
         binding.fabAddTimesheet.setOnClickListener{
-            val intent = Intent(this, GoalsActivity::class.java)
-            startActivity(intent)
+            intentHandler.openActivityIntent(TimesheetCreationActivity::class.java)
         }
 
         // Display the current week initially
-        displayCurrentWeek()
+        displayCurrentWeek(currentDate)
+        //retrieveDataFromFirestore()
     }
 
 
     private fun navPreviousWeek() {
-        // Get the current date
-        val calendar = Calendar.getInstance()
-        calendar.time = Date()
-
-        // Subtract 7 days from the current date
-        calendar.add(Calendar.DAY_OF_YEAR, -7)
-
-        // Display the new week
-        displayWeek(calendar.time)
+        //displayWeek(currentDate.minusDays(7))
     }
 
     private fun navNextWeek() {
-        // Get the current date
-        val calendar = Calendar.getInstance()
-        calendar.time = Date()
-
-        // Add 7 days to the current date
-        calendar.add(Calendar.DAY_OF_YEAR, 7)
-
-        // Display the new week
-        displayWeek(calendar.time)
+        displayWeek(currentDate.plusDays(7))
     }
 
-    private fun displayCurrentWeek() {
+    private fun displayWeek(endDate: LocalDate) {
         // Display the current week
-        displayWeek(Date())
+        val startDay = endDate.minusDays(7).dayOfMonth
+        binding.tvWeeklySelector.text= "${startDay} - ${formatDate(endDate)}"
     }
 
-    private fun displayWeek(date: Date) {
+    fun formatDate(date: LocalDate): String {
+        val dateFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy")
+        return date.format(dateFormatter)
+    }
+
+    private fun displayCurrentWeek(date: LocalDate?) {
         // Calculate the start date of the week
-        val startCalendar = Calendar.getInstance()
-        startCalendar.time = date
-        startCalendar.set(Calendar.DAY_OF_WEEK, startCalendar.firstDayOfWeek)
-        val startDate = startCalendar.time
-
+        val startDay = currentDate.minusDays(7).dayOfMonth
         // Calculate the end date of the week
-        val endCalendar = Calendar.getInstance()
-        endCalendar.time = startDate
-        endCalendar.add(Calendar.DAY_OF_YEAR, 6)
-        val endDate = endCalendar.time
-
-        //Setting the output format of date
-        val dateFormat = SimpleDateFormat("d", Locale.getDefault())
-        val startDay = dateFormat.format(startDate)
-        val endDay = dateFormat.format(endDate)
-
-        val monthFormat = SimpleDateFormat("MMMM yy'", Locale.getDefault())
-        val monthYear = monthFormat.format(startDate)
-
+        val endDate = currentDate
         // Update UI with the new week time window using data binding
-        binding.tvWeeklySelector.setText("$startDay - $endDay $monthYear")
+        binding.tvWeeklySelector.text= "${startDay} - ${formatDate(endDate)}"
     }
+
+    private fun retrieveDataFromFirestore() {
+        // Reference to the project document in Firestore
+        val timesheetRef = firestoreInstance.collection("timesheets").document("UvZGFdEzfGM7KT4lUwy3")
+
+        // Retrieve data from Firestore
+        timesheetRef.get()
+            .addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    // Parse and populate the TextViews with data
+                    val timesheetUser = documentSnapshot.getString("timesheetUser")
+                    val timesheetName = documentSnapshot.getString("timesheetName")
+                    val timesheetProjectName = documentSnapshot.getString("timesheetProjectName")
+                    val timesheetStartDate = documentSnapshot.getString("timesheetStartDate")
+                    val timesheetStartTime = documentSnapshot.getString("timesheetStartTime")
+                    val timesheetEndTime = documentSnapshot.getString("timesheetEndTime")
+                    val timesheetDescription = documentSnapshot.getString("timesheetDescription")
+
+                    // Update TextViews with retrieved data
+                    val tvProjectName = findViewById<TextView>(R.id.tv_ProjectName)
+                    val tvStartTime = findViewById<TextView>(R.id.tv_EndTime)
+                    val tvEndTime = findViewById<TextView>(R.id.tv_colour)
+                    val tvTimesheetRecentdate = findViewById<TextView>(R.id.tv_timesheet_recent_date)
+                    val tvTimesheetdescription = findViewById<TextView>(R.id.tv_TimesheetDescription)
+
+                    tvProjectName.text = timesheetProjectName
+                    tvStartTime.text = timesheetStartTime
+                    tvEndTime.text = timesheetEndTime
+                    tvTimesheetRecentdate.text = timesheetStartDate
+                    tvTimesheetdescription.text = timesheetDescription
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Log any errors that occur while retrieving data
+                Log.e("ProjectDetailsActivity", "Error retrieving project data: $exception")
+            }
+    }
+
 }
 
