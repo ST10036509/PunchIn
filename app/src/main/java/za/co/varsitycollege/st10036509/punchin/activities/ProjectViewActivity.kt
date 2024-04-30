5112d0d2c89f4c0a3d98c9e9669ec18a5ca4e90b
Leonard <st10026396@vcconnect.edu.za>
2024-04-30 11:13:57 +0200

    OS-175 : Added default data to the scroll view in Project View page.

--- app/src/main/java/za/co/varsitycollege/st10036509/punchin/activities/ProjectViewActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10036509/punchin/activities/ProjectViewActivity.kt
@@ -5,6 +5,7 @@ CREATED: 29/04/2024
 LAST MODIFIED: 29/04/2024
 */
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import za.co.varsitycollege.st10036509.punchin.databinding.ActivityProjectViewBinding
 
@@ -17,7 +18,30 @@ class ProjectViewActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         binding =  ActivityProjectViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
 
+        // Dummy data for demonstration
+        val names = arrayOf("Project 1", "Project 2", "Project 3")
+        val timesheets = arrayOf("10", "15", "20")
+        val hoursLogged = arrayOf("50", "75", "100")
+
+        binding.apply{
+
+            // Combine the data into a single string
+            val combinedText = StringBuilder()
+            for (i in names.indices) {
+                combinedText.append("Name: ${names[i]}, Timesheets: ${timesheets[i]}, Hours Logged: ${hoursLogged[i]}\n")
+            }
+
+            // Create a TextView and set its text to the combined string
+            val textView = TextView(this@ProjectViewActivity)
+            textView.text = combinedText.toString()
+
+            // Get reference to the LinearLayout inside ScrollView
+            val linearLayout = svProjects
+
+            // Add the TextView to the LinearLayout
+            linearLayout.addView(textView)
+        }
+    }
 }
 
