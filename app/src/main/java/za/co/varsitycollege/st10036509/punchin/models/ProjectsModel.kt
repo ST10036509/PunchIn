7c96c9b771a33080d6b4873519f921c1210e3e60
Leonard <st10026396@vcconnect.edu.za>
2024-05-01 11:14:46 +0200

    OS-184 : Implemented ProjectsModel to write to the database and designed structure of projects to be used in the future.

--- app/src/main/java/za/co/varsitycollege/st10036509/punchin/models/ProjectsModel.kt
+++ app/src/main/java/za/co/varsitycollege/st10036509/punchin/models/ProjectsModel.kt
@@ -2,55 +2,89 @@ package za.co.varsitycollege.st10036509.punchin.models
 /*
 AUTHOR: Leonard Bester
 CREATED: 30/04/2024
-LAST MODIFIED: 30/04/2024
+LAST MODIFIED: 1/05/2024
  */
 
 /*
 Model of Projects designed to handle variables and data specific
 functions and variables
  */
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+
 class ProjectsModel (
-    var totalHours: String,
-    var totalTimesheets: String,
-    var hourlyRate: String,
-    var totalEarnings: String,
-    var projectColor: String,
+    var projectName: String,
     var organizationName: String,
-    var timesheetsList: String,
-    var projectDescription: String,
-) { // Beyond this point is heavy on chatGPT
+    var startDate: String,
+    var setColor: String,
+    var hourlyRate: String,
+    var description: String,
+    var totalTimeSheets: String,
+    var totalHours: String,
+    var totalEarnings: String,
+    var userId: String
+) {
     // Method to set project data
     fun setData(
-        totalHours: String,
-        totalTimesheets: String,
-        hourlyRate: String,
-        totalEarnings: String,
-        projectColor: String,
+        projectName: String,
         organizationName: String,
-        timesheetsList: String,
-        projectDescription: String
+        startDate: String,
+        setColor: String,
+        hourlyRate: String,
+        description: String,
+        totalTimeSheets: String,
+        totalHours: String,
+        totalEarnings: String,
+        userId: String
     ) {
-        this.totalHours = totalHours
-        this.totalTimesheets = totalTimesheets
-        this.hourlyRate = hourlyRate
-        this.totalEarnings = totalEarnings
-        this.projectColor = projectColor
+        this.projectName = projectName
         this.organizationName = organizationName
-        this.timesheetsList = timesheetsList
-        this.projectDescription = projectDescription
+        this.startDate = startDate
+        this.setColor = setColor
+        this.hourlyRate = hourlyRate
+        this.description = description
+        this.totalTimeSheets = totalTimeSheets
+        this.totalHours = totalHours
+        this.totalEarnings = totalEarnings
+        this.userId = userId
     }
 
     // Method to get project data as a map
-    fun getData(): Map<String, String> {
+    fun getData(): Map<String, Any> {
         return mapOf(
-            "totalHours" to totalHours,
-            "totalTimesheets" to totalTimesheets,
-            "hourlyRate" to hourlyRate,
-            "totalEarnings" to totalEarnings,
-            "projectColor" to projectColor,
+            "projectName" to projectName,
             "organizationName" to organizationName,
-            "timesheetsList" to timesheetsList,
-            "projectDescription" to projectDescription
+            "startDate" to startDate,
+            "setColor" to setColor,
+            "hourlyRate" to hourlyRate,
+            "description" to description,
+            "totalTimeSheets" to totalTimeSheets,
+            "totalHours" to totalHours,
+            "totalEarnings" to totalEarnings,
+            "userId" to userId
         )
+    }
+
+    // Method to write project data to Firestore
+    fun writeDataToFirestore() {
+        // Access Firestore instance
+        val firestore = FirebaseFirestore.getInstance()
+
+        // Define the Firestore collection
+        val collection = firestore.collection("projects")
+
+        // Get project data as a map
+        val projectData = getData()
+
+        // Add the project data to Firestore
+        collection.add(projectData)
+            .addOnSuccessListener { documentReference ->
+                // Data successfully stored in Firestore
+                Log.d("ProjectsModel", "Project data stored successfully. Document ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                // Error storing data in Firestore
+                Log.e("ProjectsModel", "Error storing project data: $e")
+            }
     }
 }
