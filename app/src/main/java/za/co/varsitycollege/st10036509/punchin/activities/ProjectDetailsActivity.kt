2f44cf3497108c9ab87d3dc5f11a5fc63a2c2170
Leonard <st10026396@vcconnect.edu.za>
2024-05-02 16:46:41 +0200

    OS-205: Return works in Project Creation

--- app/src/main/java/za/co/varsitycollege/st10036509/punchin/activities/ProjectDetailsActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10036509/punchin/activities/ProjectDetailsActivity.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import za.co.varsitycollege.st10036509.punchin.databinding.ActivityProjectDetailsBinding
 import za.co.varsitycollege.st10036509.punchin.models.ProjectsModel
+import za.co.varsitycollege.st10036509.punchin.utils.IntentHandler
 import za.co.varsitycollege.st10036509.punchin.utils.LoadDialogHandler
 import za.co.varsitycollege.st10036509.punchin.utils.ToastHandler
 
@@ -22,6 +23,7 @@ class ProjectDetailsActivity : AppCompatActivity() {
     private lateinit var toaster: ToastHandler
     private var progressDialog: ProgressDialog? = null//create a loading dialog instance
     private lateinit var loadingDialogHandler: LoadDialogHandler//setup an intent handler for navigating pages
+    private lateinit var intentHandler: IntentHandler//setup an intent handler for navigating pages
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +31,10 @@ class ProjectDetailsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         loadingDialogHandler = LoadDialogHandler(this@ProjectDetailsActivity, progressDialog)//initialise the loading dialog
-
+        intentHandler = IntentHandler(this@ProjectDetailsActivity)//setup an intent handler for navigating pages
         // Initialize FirebaseFirestore instance
         firestore = FirebaseFirestore.getInstance()
+
 
         toaster = ToastHandler(this@ProjectDetailsActivity)
 
@@ -64,6 +67,14 @@ class ProjectDetailsActivity : AppCompatActivity() {
                 toaster.showToast("No project found...")
             }
         }
+
+        // Find the return button and set OnClickListener
+        binding.btnReturn.setOnClickListener {
+            toaster.showToast("Returning")
+            intentHandler.openActivityIntent(ProjectViewActivity::class.java)
+        }
+
+
     }
 }
 //______________________....oooOO0_END_OF_FILE_0OOooo....______________________\\
